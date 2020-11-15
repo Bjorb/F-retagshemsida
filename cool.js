faf05d36f29a46a3e5837cb3a6d0afe63f897507
Björn Kökeritz <bjorn.kokeritz@learnet.se>
2020-11-15 12:17:51 +0100

    first commit

---
+++ cool.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById('frontPage').style.height = '17vh'
+   document.getElementById('text').style.top = '-60vh'
+   document.getElementById('close').style.top = '-35vh'
+}
+
+
+function closeNav() {
+  document.getElementById('frontPage').style.height = '100vh'
+  document.getElementById('text').style.top = '15vh'
+   document.getElementById('close').style.top = '42vh'
+}
